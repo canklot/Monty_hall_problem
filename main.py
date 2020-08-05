e178063e7deb97a9eb89cd69b3bb00ec85ddf726
canklot <33100414+canklot@users.noreply.github.com>
2020-08-05 13:45:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+    
+def Game(change):
+
+    doors = {'a':9 , 'b':9 ,'c':9}
+    Prizes = [0, 1, 0]
+    
+    #Place random prize at doors
+    for d in doors:
+      myrandom = random.randint(0 , len(Prizes)-1)
+      doors[d] = Prizes.pop(myrandom)
+    
+    #player choose a random door 
+    player = random.choice(list(doors))
+    
+    #Reveal a unchoosen empty door 
+    while True:
+      revealedDoor = random.choice(list(doors))
+      if revealedDoor != player and doors[revealedDoor] == 0 :
+          doors.pop(revealedDoor)
+          break
+      
+    #player change the door  
+    if change:
+        for x in range(len(doors)):
+            if list(doors)[x] != player:
+                player = list(doors)[x]
+                break
+        
+    #Retrun win or loose
+    if doors[player] == 1:
+        return("win")
+        
+    elif doors[player] == 0:
+        return("loose")
+#-----------------------------------------------------
+
+#Store results in a list
+results = []
+for x in range(10000):
+    results.append(Game(1)) 
+#print(*results)
+
+winCount = results.count("win")
+looseCount = results.count("loose")
+
+print("winCount   = " , winCount)
+print("looseCount = " ,looseCount)
+
